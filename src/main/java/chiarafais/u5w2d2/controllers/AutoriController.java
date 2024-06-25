98a2dcb2264a4c3757e9df70630361e26cd87c7a
chiarafais <chiara0915@gmail.com>
2024-06-25 14:12:07 +0200

    autori controller and blog post controller + testing on postman

--- src/main/java/chiarafais/u5w2d2/controllers/AutoriController.java
+++ src/main/java/chiarafais/u5w2d2/controllers/AutoriController.java
@@ -1,9 +1,48 @@
 package chiarafais.u5w2d2.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import chiarafais.u5w2d2.entities.AutoreBlogPost;
+import chiarafais.u5w2d2.services.AutoriService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/authors")
 public class AutoriController {
+    @Autowired
+    private AutoriService autoriService;
+
+    // 1. GET http://localhost:3001/authors (ritorna la lista di autori) //FUNZIONA
+    @GetMapping
+    private List<AutoreBlogPost> getAllAutori(){
+        return this.autoriService.getAutoriList();
+    }
+
+    // 2. GET http://localhost:3001/authors/{authorId} (ritorna un singolo autore) //FUNZIONA
+    @GetMapping("/{authorId}")
+    private AutoreBlogPost findAutoreById(@PathVariable int authorId){
+        return this.autoriService.findById(authorId);
+    }
+
+    // 3. POST http://localhost:3001/authors (+ body) (crea un nuovo autore) //FUNZIONA
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private AutoreBlogPost saveAutore(@RequestBody AutoreBlogPost body){
+        return this.autoriService.saveAutore(body);
+    }
+
+    // 4. PUT http://localhost:3001/authors/{authorId} (+ body) (modifica lo specifico autore) //FUNZIONA
+    @PutMapping("/{authorId}")
+    private AutoreBlogPost findByIdAndUpdate(@PathVariable int authorId, @RequestBody AutoreBlogPost body){
+        return this.autoriService.findByIdAndUpdate(authorId, body);
+    }
+
+    // 5. DELETE http://localhost:3001/authors/{authorId} (cancella lo specifico autore) //FUNZIONA
+    @DeleteMapping("/{authorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // Status Code 204
+    private void findAutoreByIdAndDelete(@PathVariable int authorId){
+        this.autoriService.findByIdAndDelete(authorId);
+    }
 }
--- src/main/java/chiarafais/u5w2d2/controllers/BlogPostsController.java
+++ src/main/java/chiarafais/u5w2d2/controllers/BlogPostsController.java
@@ -1,4 +1,48 @@
 package chiarafais.u5w2d2.controllers;
 
+import chiarafais.u5w2d2.entities.BlogPost;
+import chiarafais.u5w2d2.services.BlogPostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/blogPosts")
 public class BlogPostsController {
+    @Autowired
+    private BlogPostsService blogPostsService;
+
+    // 1. GET http://localhost:3001/blogPosts (ritorna la lista di blog posts)
+    @GetMapping
+    private List<BlogPost> getAllBlogPosts(){
+        return this.blogPostsService.getBlogPostsList();
+    }
+
+    // 2. GET http://localhost:3001/blogPosts/{blogPostId} (ritorna un blog post)
+    @GetMapping("/{blogPostId}")
+    private BlogPost findBlogPostById(@PathVariable int blogPostId){
+        return this.blogPostsService.findById(blogPostId);
+    }
+
+    // 3. POST http://localhost:3001/blogPosts (+ body) (crea un nuovo blog post)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private BlogPost saveBlogPost(@RequestBody BlogPost body){
+        return this.blogPostsService.saveBlogPost(body);
+    }
+
+    // 4. PUT http://localhost:3001/blogPosts/{blogPostId} (+ body) (modifica lo specifico blog post)
+    @PutMapping("/{blogPostId}")
+    private BlogPost findBlogPostByIdAndUpdate(@PathVariable int blogPostId, @RequestBody BlogPost body){
+        return this.blogPostsService.findByIdAndUpdate(blogPostId, body);
+    }
+
+    // 5. DELETE http://localhost:3001/blogPosts/{blogPostId} (cancella lo specifico blog post)
+    @DeleteMapping("/{blogPostId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // Status Code 204
+    private void findBlogPostByIdAndDelete(@PathVariable int blogPostId){
+        this.blogPostsService.findByIdAndDelete(blogPostId);
+    }
 }
